9601012c761c4c3dcf47968b1fdc7c01da3e24fc
ElvinIsmayil <elvinismayil2007@gmail.com>
2024-07-02 12:29:58 +0400

    text.js added

---
+++ theme/js/text.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    new Typed('#typed-slogan', {
+      strings: ["Simplifying Meetings, Amplifying Results"],
+      typeSpeed: 50,
+      startDelay: 500,
+      backSpeed: 25,
+      backDelay: 5000,
+      loop: true,
+      showCursor: false
+    });
+  });
