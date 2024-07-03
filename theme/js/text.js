efe70eff12bc69d9a4eb8a5bb6b098d22dfd13fc
ElvinIsmayil <elvinismayil2007@gmail.com>
2024-07-04 02:24:13 +0400

    Animations and other changes

--- theme/js/text.js
+++ theme/js/text.js
@@ -1,11 +1,15 @@
-document.addEventListener("DOMContentLoaded", function() {
-    new Typed('#typed-slogan', {
-      strings: ["Simplifying Meetings, Amplifying Results"],
-      typeSpeed: 50,
-      startDelay: 500,
-      backSpeed: 25,
-      backDelay: 5000,
-      loop: true,
-      showCursor: false
-    });
-  });
+AOS.init({
+  duration: 1000,
+  once: true,
+});
+
+// Initialize Typed.js
+var typed = new Typed("#typed-slogan", {
+  strings: ["Accessible learning, Anytime, Anywhere", "Innovative.", "Reliable.", "Student-friendly.", "Tutor-friendly."],
+  typeSpeed: 50,
+  backSpeed: 30,
+  backDelay: 3000, // Time before backspacing
+  startDelay: 1000, // Time before typing starts
+  loop: true,
+  showCursor: false // Hide the cursor
+});
